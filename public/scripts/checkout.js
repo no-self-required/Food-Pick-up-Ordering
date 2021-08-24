6e53454721dd758574d76f9ae9fab9c8e69ccfb7
Taeyeong Nam <ellienjuly@gmail.com>
2021-08-24 23:25:29 +0000

    Delete view-saved-order.js

--- public/scripts/checkout.js
+++ public/scripts/checkout.js
@@ -1,4 +1,29 @@
 $(() => {
-  $.post('/checkout')
-  $()
+  $('.menu-submit').on('click', '.menu-listing', function () {
+
+    console.log($(this).attr("data-id"));
+    let menuID = $(this).attr("data-id");
+
+    let cart = localStorage.getItem('myCart');
+    if (!cart) {
+      cart = [];
+    } else {
+      cart = JSON.parse(cart);
+    }
+
+    $.post("/users", {
+      menuID
+    }, function (data) {
+      // console.log("DATA----", data);
+      let cartItem = {
+        name: data[0].name,
+        price: data[0].price
+      }
+      cart.push(cartItem);
+      localStorage.setItem('myCart', JSON.stringify(cart));
+      $('#cart-items').append(inCartList.addItem(cartItem));
+      Cart.renderCart()
+      Cart.calculateTotal(data[0].price);
+    });
+  })
 })
